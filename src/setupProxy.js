85eb9a802daa446c01194bd076ff3dc4a059b080
kellervl <44319134+kellervl@users.noreply.github.com>
2019-11-22 21:56:19 +0300

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/auth', { target: 'http://localhost:8000/' }));
+  app.use(proxy('/auth', { target: 'http://35.228.34.97:8000/' }));
 };
